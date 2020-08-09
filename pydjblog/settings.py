1812e337a7f2f583ca9ca94d39a7d9b9e00b1165
pratik <pratik.bidkar@gmail.com>
2020-08-09 12:05:42 +0530

    updated settings.py with import django heroku and activate django heroku

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,3 +144,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pratik.bidkar@gmail.com'
 EMAIL_HOST_PASSWORD = 'pratik123'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
